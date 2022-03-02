f1f3f0c8245c96277fb82641654191976dba0238
subhamay2002 <subhamaynath2002@gmail.com>
2022-03-02 20:21:37 +0530

    product added

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,38 @@
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 export default function Home(props) {
-//  console.log('hello');
+  const listing = props.products.map((product, index) => {
+    return (
+      <div className="card cardwidth" key={product._id}>
+      <img src={product.image} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">{product.name}</h5>
+        <p className="card-text">{product.description}</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+    )}
+  )
 
   return (
     <div className={styles.container}>
       
-      {/* <h1 className='bg-primary'>{props.data}</h1> */}
+      {
+        listing
+      }
       
     </div>
   )
 }
-// export async function getStaticProps(context) {
-//   const res=await fetch('http://localhost:3000/api/products')
-//   const data= await res.json()
-//   console.log(data)
-//   return {
-//     props: {data: data.name}, // will be passed to the page component as props
-//   }
-// }
+export async function getStaticProps(context) {
+  const res=await fetch('http://localhost:3000/api/products')
+  const data= await res.json()
+  // console.log(data)
+  return {
+    props: {
+      products: data
+    }
+  }
+}
 
