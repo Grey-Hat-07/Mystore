64998d5203fc23af5bc5a3f3a5c5a77edf6486bb
subhamay2002 <subhamaynath2002@gmail.com>
2022-03-04 20:43:02 +0530

    card

--- pages/index.js
+++ pages/index.js
@@ -2,39 +2,43 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 export default function Home(props) {
   var listing = props.products.map((product, index) => {
-    if(product.description.length > 100) {
+    if (product.description.length > 100) {
       product.description = product.description.substring(0, 40) + '...';
     }
     return (
-      <div className="card  col-md-3 mb-2 ms-2" key={product._id}>
+      <div className="col-md-3 mb-2" key={product._id}>
         <Link
-        href={'/productDetail/[id]'}
-        as={`/productDetail/${product._id}`} 
+          href={'/productDetail/[id]'}
+          as={`/productDetail/${product._id}`}
         ><div>
-      <img src={product.image} className="card-img-top imgover" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">{product.name}</h5>
-        <p className="card-text">{product.description}</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
-      </div></div></Link>
-    </div>
+            <div className="card cardstyle cardhover">
+              <img src={product.image} className="card-img-top" alt={product.name} height="200" />
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                {/* <p className="card-text text-muted">{category}</p> */}
+                <p className="card-text">₹{product.price}</p>
+                {/* <p className="card-text">⭐{rating.rate}</p> */}
+              </div></div>
+          </div></Link>
+      </div>
 
-    )}
+    )
+  }
   )
 
   return (
     <div className={styles.container}>
       <div className='row'>
-      {
-        listing
-      }
+        {
+          listing
+        }
       </div>
     </div>
   )
 }
 export async function getStaticProps(context) {
-  const res=await fetch('http://localhost:3000/api/products')
-  const data= await res.json()
+  const res = await fetch('http://localhost:3000/api/products')
+  const data = await res.json()
   return {
     props: {
       products: data
--- pages/productDetail/[id].js
+++ pages/productDetail/[id].js
@@ -1,25 +1,31 @@
-const productDetails =(props)=>{
-    const {product}=props;
-    return(
-        <div className="card mb-2 ms-2">
-        <img src={product.image} className="card-img-top imgover" alt="..."/>
-        <div className="card-body">
-        <h5 className="card-title">{product.name}</h5>
-        <p className="card-text">{product.description}</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+const productDetails = (props) => {
+    const { product } = props;
+    return (
+        <div className="row container">
+            <div className="col-md-6">
+                <img src={product.image} className="card-img-top" alt={product.name} height="400" />
+            </div>
+            <div className="col-md-6">
+                <h3>{product.name}</h3>
+                {/* <p className='text-muted'>{products[productid - 1].category}</p> */}
+
+                <p>₹{product.price}</p>
+                {/* <p>⭐{products[productid - 1].rating.rate}&emsp;
+                    {products[productid - 1].rating.count} Ratings</p>*/}
+                <p>{product.description}</p> 
+            </div>
         </div>
-    </div>
     )
 }
 
-export async function getServerSideProps({params:{id}}) {
-    const res=await fetch(`http://localhost:3000/api/product/${id}`)
-    const data= await res.json()
+export async function getServerSideProps({ params: { id } }) {
+    const res = await fetch(`http://localhost:3000/api/product/${id}`)
+    const data = await res.json()
     return {
         props: {
             product: data
         }
     }
-    
+
 }
 export default productDetails;
