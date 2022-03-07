9461e4b58ad6bd7b357262dc4e335850e4f42abf
subhamay2002 <subhamaynath2002@gmail.com>
2022-03-07 21:01:29 +0530

    delete added

--- pages/api/product/[pid].js
+++ pages/api/product/[pid].js
@@ -1,8 +1,23 @@
 import product from '../../../Models/product' ;
  
  export default async (req,res) => {
+     switch(req.method){
+            case 'GET':
+                getproduct(req,res);
+                break;
+            case 'DELETE':
+                deleteproduct(req,res);
+                break;
+     }
+
+}
+const getproduct= async (req,res) => {
     const {pid}=req.query;
     const item=await product.findOne({_id:pid});
     res.status(200).json(item);
-
+}
+const deleteproduct= async (req,res) => {
+    const {pid}=req.query;
+    await product.findOneAndDelete({_id:pid});
+    res.status(200).json({});
 }
--- pages/productDetail/[id].js
+++ pages/productDetail/[id].js
@@ -19,41 +19,61 @@ const productDetails = (props) => {
                 <p>₹{product.price}</p>
                 <p>{product.description}</p>
                 <div className="input-group mb-3">
-                    <input type="number" className="form-control" min={1}/>
-                    <button className="btn btn-primary" type="button" id="button-addon1">
+                    <input type="number" className="form-control" min={1} />
+                    <button className="btn btn-warning" type="button" id="button-addon1">
                         Addtocart<Addtocart /></button>
                 </div>
-                <button className="btn btn-danger mt-2 ms-3" type="button" id="button-addon2" onClick={getModalStyle}>
-                    <Delete />Delete</button>
+                <GetModalStyle product={product} />
             </div>
         </div>
     )
 }
-const getModalStyle = () => {
-    return(
-        <div className="modal" tabindex="-1">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title">Modal title</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+const GetModalStyle = ({product}) => {
+    const router = useRouter();
+    const deleteproduct = async () => {
+        const res = await fetch(`${baseUrl}/api/product/${product._id}`, {
+            method: "DELETE"})
+        await res.json()
+        router.push("/")
+     }
+    return (
+        <>  
+        {/* button trigger */}
+            <button type="button" className="btn btn-danger mt-2 ms-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <Delete />Delete
+            </button>
+
+            {/* Modal */}
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <h3>{product.name}</h3>
+                            Are you sure you want to delete this product?
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-danger"
+                            onClick={() => {deleteproduct()}}
+                            >Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
 
     )
 }
 
+
 export async function getServerSideProps({ params: { id } }) {
-    const res = await fetch(`${baseUrl}/api/product/${id}`)
+    const res = await fetch(`${baseUrl}/api/product/${id}`,{
+        method: "GET"
+    })
     const data = await res.json()
     return {
         props: {
