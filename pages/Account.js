00183de97a8f5b752a4217950656d8d0ebb624e5
subhamay2002 <subhamaynath2002@gmail.com>
2022-03-13 20:07:51 +0530

    account

--- pages/Account.js
+++ pages/Account.js
@@ -3,11 +3,20 @@ import { useEffect, useState } from 'react'
 import baseUrl from '../helpers/baseUrl'
 import { parseCookies } from 'nookies';
 export default function Account() {
+    const [data, setData] = useState({});
+    useEffect(async() => {
+     const res = await fetch(`${baseUrl}/api/account`);
+      const data = await res.json();
+      setData(data);
+
+
+    })
 
 
   return (
     <div>Account
-     
+     {data.name}
+     {data.email}
       
     </div>
   )
--- pages/api/account.js
+++ pages/api/account.js
@@ -1,8 +1,8 @@
 
 import User from "../../Models/User";
 export default async (req, res) => {
-    const { user } = parseCookies();
+    const { user } = req.cookies;
     const data = await User.findOne({ _id: user });
-    console.log(data);
+
     res.status(200).json(data);
 }
